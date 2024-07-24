835817e25b6fbaac296f6ff137d4e246c2e07a59
wyatt/gold <119985279+wyattbubbylee@users.noreply.github.com>
2024-07-24 16:47:12 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
 document.getElementById('payButton').addEventListener('click', function() {
-    alert('Thank you for your purchase!');
-    window.location.href = 'your-zip-file.zip';  // Replace 'your-zip-file.zip' with the actual path to your zip file
+    document.getElementById('cart').style.display = 'none';
+    document.getElementById('thankYouMessage').style.display = 'block';
+    setTimeout(function() {
+        window.location.href = 'your-zip-file.zip';  // Replace 'your-zip-file.zip' with the actual path to your zip file
+    }, 1000);  // Wait for 1 second before starting the download
 });
