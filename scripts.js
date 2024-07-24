89164ded622ef45fa0d113c558857cec2592245b
wyatt/gold <119985279+wyattbubbylee@users.noreply.github.com>
2024-07-24 18:13:28 -0400

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('payButton').addEventListener('click', function() {
-    document.getElementById('cart').style.display = 'none';
-    document.getElementById('thankYouMessage').style.display = 'block';
-    setTimeout(function() {
-        window.location.href = 'your-zip-file.zip';  // Replace 'your-zip-file.zip' with the actual path to your zip file
-    }, 1000);  // Wait for 1 second before starting the download
-});
