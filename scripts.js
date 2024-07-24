5ef3c50411cd63fe6f70b65bd66eec70a8b03887
wyatt/gold <119985279+wyattbubbylee@users.noreply.github.com>
2024-07-24 16:44:10 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
-document.getElementById('myButton').addEventListener('click', function() {
-    alert('Button clicked!');
+document.getElementById('payButton').addEventListener('click', function() {
+    alert('Thank you for your purchase!');
+    window.location.href = 'your-zip-file.zip';  // Replace 'your-zip-file.zip' with the actual path to your zip file
 });
